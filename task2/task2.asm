ce75ccb196fae7824279f0ad8129a3978bac991c
Yan Zhe Liu <1913961320@qq.com>
2024-10-29 18:04:06 +0800

    task2 & 3

--- task2/task2.asm
+++ task2/task2.asm
@@ -1,187 +1,131 @@
-assume cs:code, ds:data, ss:stack
+assume cs:code, ds:data
 
 data segment
-    message1 db "What's your name?",0
-    message2 db "Which class are you in?",0
-    message3 db "Your name is ",0
-    message4 db ", and your class is ",0
-    message5 db ". confirm(y/n)",0
+    message1  db "What's your name?",0dh,0ah,'$'
+    message2  db "Which class are you in?",0dh,0ah,'$'
+    message3  db "Your name is ",'$'
+    message4  db ", and your class is ",'$'
+    message5  db ". confirm(y/n)",'$'
+
+    string    db 0dh,0ah,24h
+
+    name_buf  db 20
+              db ?
+              db 20 dup(0)
+
+    class_buf db 20
+              db ?
+              db 20 dup(0)
+              
+    answer    db 0
 
 data ends
 
-stack segment
-          db 128 dup (0)
-stack ends
-
 code segment
     start:      
-                mov  ax, stack
-                mov  ss, ax
-                mov  sp, 128
-
                 mov  ax, data
                 mov  ds, ax
                 mov  si, 0
+    begin:      
+                mov  bx,0
 
-	            mov  ax, 0B800H
-	            mov  es, ax
+                mov  bh,00h
+                call B10SCRN
 
-	            mov  dl, 1
-	            mov  dh, 0
-	            mov  cl, 00000111B
-	            call show_str
-
-                call get_str
-
-	            mov  dl, 3
-	            mov  dh, 0
-	            mov  cl, 00000111B
-	            call show_str    
-
-	            mov  ax, 4c00h
-	            int  21h
-
-
-
-    show_str:   push ax
-                push bx
-                push ds
-                push es
-                push di
-                push si
-                push cx
-                push dx
-            			
-                mov  ah, 0
-                mov  al, 160
-                mov  dh, 0
-                mov  bx, dx
-                mov  dx, 0
-                mul  bx
-                pop  dx
-                mov  dh, 0
-                add  ax, dx; 注意这里加两遍，不能简单地将列号相加，因为显示一个字符用了两个字节
-                add  ax, dx
-                mov  bx, ax
-    ; 确定输出的位置
-    _showStr:   mov  cx, 0
-                mov  cl, ds:[si + 0]
-                jcxz _retShowStr
-                mov  es:[bx + 0], cl
-                pop  cx
-                push cx
-                mov  byte ptr es:[bx + 1], cl
-                inc  si
-                add  bx, 2
-                jmp  _showStr
-
-    _retShowStr:pop  cx
-                pop  si
-                pop  di
-                pop  es
-                pop  ds
-                pop  bx
-                pop  ax
-                ret
-
-    get_str:    push ax
-    _getStrs:   
-                mov  ah, 0
-                int  16h
-                cmp  al, 20h                      ; ASCII码小于20h说明不是字符
-                jb   nochar
-                mov  ah, 0
-                call char_stack                   ; 字符入栈
-                mov  ah, 2
-                call char_stack                   ; 显示栈中字符
-                jmp  _getStrs
-
-    nochar:     cmp  ah, 0eh                      ; 退格键的扫描码
-                je   _backspace
-                cmp  ah, 1ch                      ; Enter 的扫描码
-                je   _enter
-                jmp  _getStrs
-
-    _backspace: 
-                mov  ah, 1
-                call char_stack                   ; 字符出栈
-                mov  ah, 2
-                call char_stack
-                jmp  _getStrs
-
-    _enter:     mov  al, 0
-                mov  ah, 0
-                call char_stack                   ; 0 入栈
-                mov  ah, 2
-                call char_stack                   ; 显示栈中字符串
-                pop  ax
-                ret
-
-
-               
-    char_stack: jmp  _charStart
-    table       dw   _push, _pop, _show
-    top         dw   0                            ; 栈顶
-
-    _charStart: push bx
-                push dx
-                push di
-                push es
-
-
-                cmp  ah, 2
-                ja   _ret
-                mov  bl, ah
-                mov  bh, 0
-                add  bx, bx
-                jmp  word ptr table[bx]
-
-    _push:      mov  bx, top
-                mov  [si][bx], al
-                inc  top
-                jmp  _ret
-
-    _pop:       cmp  top, 0
-                je   _ret
-                dec  top
-                mov  bx, top
-                mov  al, [si][bx]
-                jmp  _ret
-
-    _show:      mov  bx, 0b800h
-                mov  es, bx
-                mov  al, 160
-                mov  ah, 0
-                mul  dh
-                mov  di, ax
-                add  dl, dl
-                mov  ah, 0
-                add  di, ax
-
-                mov  bx, 0
-
-    _shows:     cmp  bx, top
-                jne  _noempty
-                mov  byte ptr es:[di], ' '
-                jmp  _ret
-	
-    _noempty:   mov  al, [si][bx]
-                mov  es:[di], al
-                mov  byte ptr es:[di + 2], ' '
-                inc  bx
-                add  di, 2
-                jmp  _shows
-
-    _ret:       pop  es
-                pop  di
-                pop  dx
-                pop  bx
-                ret
-
-
-
-
-                mov  ax,4c00h
+                mov  ah,09h
+                lea  dx,message1
                 int  21h
+
+    input_name: lea  dx,name_buf
+                mov  ah,0ah
+                int  21h
+                cmp  name_buf+1, 0
+                je   input_name
+
+                mov  bl, name_buf+1
+                mov  bh, 0
+                mov  BYTE PTR[name_buf+bx+2],24h
+
+                lea  dx,string
+                mov  ah,09h
+                int  21h
+
+                mov  ah,09h
+                lea  dx,message2
+                int  21h
+
+    input_class:mov  ah,0ah
+                lea  dx,class_buf
+                int  21h
+                cmp  class_buf+1, 0
+                je   input_class
+
+                mov  bl, class_buf+1
+                mov  bh, 0
+                mov  BYTE PTR[class_buf+bx+2],24h
+
+                lea  dx,string
+                mov  ah,09h
+                int  21h
+
+                mov  ah,09h
+                lea  dx,message3
+                int  21h
+
+                mov  ah,09h
+                lea  dx,name_buf+2
+                int  21h
+
+                mov  ah,09h
+                lea  dx,message4
+                int  21h
+
+                mov  ah,09h
+                lea  dx,class_buf+2
+                int  21h
+                
+                mov  ah,09h
+                lea  dx,message5
+                int  21h
+
+                lea  dx,string
+                mov  ah,09h
+                int  21h
+
+    Confirm:    
+                mov  ah,01h
+                int  21h
+
+                mov  answer, al
+
+                cmp  answer, 'n'
+                je   begin
+
+                cmp  answer, 'N'
+                je   begin
+
+                cmp  answer, 'y'
+                je   exit
+
+                cmp  answer, 'Y'
+                je   exit
+
+                jmp  Confirm
+
+    exit:       mov  ax, 4c00h
+                int  21h
+
+B10SCRN proc near
+                mov  ax,0600h
+                mov  bh,07h
+                mov  cx,0000h
+                mov  dx,184fh
+                int  10h
+                ret
+B10SCRN endp
+
+
+
+
 code ends
 end start
